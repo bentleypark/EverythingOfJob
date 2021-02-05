361e802816597baef42f29fb85c42c4a6665b3c7
Bentley Park <junggil.park@sovoro.kr>
2021-02-06 00:29:35 +0900

    first commit

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,83 @@
+object Versions {
+
+    // Class Path for Plugin
+    const val buildGradleVersion = "4.1.2"
+    const val googleServiceVersion = "4.3.4"
+    const val sentryGradleVersion = "1.7.36"
+    const val r8Version = "2.1.75"
+    const val androidJunit5Version = "1.6.2.0"
+    const val androidMavenGradleVersion = "2.1"
+    const val detektGradleVersion = "1.15.0"
+    const val navSafArgPluginVersion = "2.3.2"
+
+    // Kotlin
+    const val kotlinVersion = "1.4.30"
+
+    // Coroutines
+    const val androidVersion = "1.4.2"
+
+    // RxJava
+    const val rxJavaVersion = "2.2.20"
+    const val rxAndroidVersion = "2.1.1"
+
+    const val junitVersion = "4.13.1"
+
+    // AndroidX
+    const val appCompatVersion = "1.2.0"
+    const val constraintLayoutVersion = "2.0.4"
+    const val coreKtxVersion = "1.3.2"
+    const val legacySupportVersion = "1.0.0"
+    const val activityVersion = "1.2.0-rc01"
+    const val fragmentVersion = "1.3.0-rc01"
+    const val recyclerviewVersion = "1.1.0"
+    const val viewpager2Version = "1.1.0-alpha01"
+    const val swipeRefreshLayoutVersion = "1.1.0"
+    const val databindingVersion = "4.1.2"
+
+    // Jetpack
+    const val workVersion = "2.4.0"
+    const val lifeCycleVersion = "2.2.0"
+    const val roomVersion = "2.2.6"
+    const val navigationVersion = "2.3.3"
+    const val startupVersion = "1.0.0"
+
+    const val testVersion = "1.3.0"
+    const val espressoVersion = "3.3.0"
+    const val junitExtVersion = "1.1.2"
+
+    // Google
+    const val materialVersion = "1.3.0"
+    const val gsonVersion = "2.8.6"
+    const val firebaseAnalyticsVersion = "17.5.0"
+    const val playCoreVersion = "1.8.1"
+
+    // Square
+    const val retrofitVersion = "2.9.0"
+    const val moshiVersion = "1.10.0"
+    const val okhttp3Version = "4.9.1"
+    const val leakcanaryVersion = "2.6"
+
+    // RxBinding
+    const val rxbindingVersion = "3.1.0"
+
+    // Lottie
+    const val lottieVerison = "3.4.3"
+
+    // Debug
+    const val timberVersion = "4.7.1"
+    const val stethoVersion = "1.5.1"
+    const val sentryVersion = "4.1.0"
+
+    // Klint
+    const val klintVersion = "0.40.0"
+
+    // Hilt
+    const val hiltVersion = "2.31.2-alpha"
+    const val hiltViewModel = "1.0.0-alpha02"
+
+    // Admob
+    const val admobVersion = "19.7.0"
+
+    // Coil
+    const val coilVersion = "1.1.1"
+}
